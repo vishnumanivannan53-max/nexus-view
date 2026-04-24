22443633964ea4604ce5d68d3cdd1519cb7ad57c
vishnu M <vishnumanivannan53@gmail.com>
2026-04-24 22:25:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider, Show, SignInButton, UserButton } from "@clerk/nextjs";
 
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Nexus View",
+  description: "All-in-one placement management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={cn("antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      >
+        <body>
+          {/* <Show when={'signed-in'}>
+            <UserButton/>
+          </Show>
+          <Show when={'signed-out'}>
+            <SignInButton/>
+          </Show> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
